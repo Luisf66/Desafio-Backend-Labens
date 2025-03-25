883acf0203961becb82d67dfd90e924bbab1e93c
Luisf66 <Luisf311220@gmail.com>
2025-03-25 09:16:49 -0300

    feat: validação de data no serializer

--- task/serializers.py
+++ task/serializers.py
@@ -6,3 +6,13 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
+    
+    def validate_term(self, value):
+        if value.year < 1900 or value.year > 2100:
+            raise serializers.ValidationError("O ano deve estar entre 1900 e 2100.")
+        return value
+        
+    def validate_conclusion(self, value):
+        if value.year < 1900 or value.year > 2100:
+            raise serializers.ValidationError("O ano deve estar entre 1900 e 2100.")
+        return value
